4b7ef2adcc14edda88c7f8743246b60ca9cd7b9f
Pancax <dakotasoles@gmail.com>
2019-10-19 23:40:53 -0500

    Initial Commit
    Signed-off-by: Pancax <dakotasoles@gmail.com>

--- app/src/main/java/pancax/emtechproj/MainActivity.java
+++ app/src/main/java/pancax/emtechproj/MainActivity.java
@@ -18,6 +18,7 @@ public class MainActivity extends AppCompatActivity implements OnTaskDoneListene
         connect.execute("currencies");
         Intent intent = new Intent(this, web_activity.class);
         startActivity(intent);
+
     }
 
     @Override
--- app/src/main/java/pancax/emtechproj/web_activity.java
+++ app/src/main/java/pancax/emtechproj/web_activity.java
@@ -2,21 +2,47 @@ package pancax.emtechproj;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.Base64;
+import android.webkit.WebSettings;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class web_activity extends AppCompatActivity {
-
+    private WebView myWebView;
+    private WebSettings webSettings;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        WebView myWebView = new WebView(this);
+        setUpWebView();
+    }
+    private void setUpWebView(){
+        myWebView = new WebView(this);
         setContentView(myWebView);
         String unencodedHtml =
                 "&lt;html&gt;&lt;body&gt;'%23' is the percent code for ‘#‘ &lt;/body&gt;&lt;/html&gt;";
         String encodedHtml = Base64.encodeToString(unencodedHtml.getBytes(),
                 Base64.NO_PADDING);
         myWebView.loadData(encodedHtml, "text/html", "base64");
+        webSettings = myWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        myWebView.setWebViewClient(new MyWebViewClient());
+    }
+    private class MyWebViewClient extends WebViewClient {
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            if ("www.example.com".equals(Uri.parse(url).getHost())) {
+                // This is my website, so do not override; let my WebView load the page
+                return false;
+            }
+            // Otherwise, the link is not for a page on my site, so launch another Activity that handles URLs
+            Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+            startActivity(intent);
+            return true;
+        }
     }
 }
+
